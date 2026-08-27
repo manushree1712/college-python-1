ded4865d17487aa0ac89a8394313fdee50f66901
manuuuuuuuuu17 <manushree.1712@gmail.com>
2026-08-27 10:49:59 +0530

    work

--- 02_operators.py
+++ 02_operators.py
@@ -36,11 +36,11 @@ print(x>5 or y<2) #1,0
 print(x>4 and y>3) #1,1
 print(x<5 and y>2) #0,1
 
-print(x<10 or z>5) #kind of error with or; 1,0
+#print(x<10 or z>5) #kind of error with or; 1,0
 # print(x<2 or z<2) #kind of error with or; 0,0 - error
 
 #print(y>2 and z<4) # error with and; 1,0
-print(x<5 and z>3) #error with and: 1,0
+#print(x<5 and z>3) #error with and: 1,0
 
 print(not x>5)
 print(not y>10)
