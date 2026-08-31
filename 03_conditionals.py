7a83d48babc490ccf137e8f1af5e698bd45ce5cb
manuuuuuuuuu17 <manushree.1712@gmail.com>
2026-08-31 19:16:26 +0530

    python work

---
+++ 03_conditionals.py
@@ -0,0 +1,38 @@
+# # 31st august, 2026
+
+# # if statement
+
+
+# age = int(input("enter your age:"))
+# if age >= 18:
+#     print("eligible!!")
+    
+# # if-else statement
+
+# n = int(input("enter a number: "))
+# if n%2 == 0:
+#     print("this nummber is even")
+# else:
+#     print("this number is odd")
+    
+
+#if-elif statement
+
+day = int(input("enter day: "))
+
+if day == 1:
+    print("monday")
+elif day == 2:
+    print("tuesday")
+elif day == 3:
+    print("wednesday")
+elif day == 4:
+    print("thursday")
+elif day == 5:
+    print("friday")
+elif day == 6:
+    print("saturday")
+elif day == 7:
+    print("sunday")
+else:
+    print("holiday!!!")
---
+++ 03_grading.py
@@ -0,0 +1,16 @@
+# 31 august, 2026
+
+marks = int(input("enter student marks: "))
+
+
+if 91 >=  marks <= 100 :
+    print("your grade is 'A'")
+elif 81 >=  marks <= 90 :
+# marks>= 81 and marks <=90
+    print("your grade is 'B'")
+elif 71 >=  marks <= 80 :
+    print("your grade is 'C'")
+elif 61 >=  marks <= 70 :
+    print("your grade is 'D'")
+else:
+    print("fail")
