b3f8ed5dfe1ca611b1e037a4802618968126c770
manuuuuuuuuu17 <manushree.1712@gmail.com>
2026-09-06 15:32:03 +0530

    update

--- 03_conditionals.py
+++ 03_conditionals.py
@@ -55,8 +55,51 @@
 
 
 
-num2 = int(input("enter: "))
-if num2%2 == 0:
-    print("even number")
+# num2 = int(input("enter: "))
+# if num2%2 == 0:
+#     print(f"{num2} is an even number")
+# else:
+#     print(f"{num2} is an odd number")
+#     # print(num2, "is an odd number")
+    
+    
+    
+# if condition:
+        #code
+# elif condition:
+        # code
+# else:
+        #code
+        
+# marks = int(input("enter marks:"))
+
+# if marks >= 90:
+#     print("A grade")
+# elif 75 <= marks <= 89:
+#     print("B grade")
+# elif 60 <= marks <= 74:
+#     print("C grade")
+# elif 40 <= marks <= 59:
+#     print("D grade")
+# else:
+#     print("FAIL")
+
+
+DAY = int(input("enter a number: "))
+
+if DAY == 1:
+    print(DAY, "monday")
+elif DAY == 2:
+    print(DAY, "tuesday")
+elif DAY == 3:
+    print(DAY, "wednesday")
+elif DAY == 4:
+    print(DAY, "thursday")
+elif DAY == 5:
+    print(DAY, "friday")
+elif  DAY == 6:
+    print(DAY, "saturday")
+elif DAY == 7:
+    print(DAY, "sunday")
 else:
-    print("odd number")
+    print("invalid input")
