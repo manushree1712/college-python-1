0ccaf3eae0f96761b9b1b146eaa303459723b515
manuuuuuuuuu17 <manushree.1712@gmail.com>
2026-09-09 14:08:22 +0530

    update

--- 03_conditionals.py
+++ 03_conditionals.py
@@ -1,88 +1,88 @@
-# # 31st august, 2026
+# 31st august, 2026
 
-# # if statement
+# if statement
 
 
-# age = int(input("enter your age:"))
-# if age >= 18:
-#     print("eligible!!")
+age = int(input("enter your age:"))
+if age >= 18:
+    print("eligible!!")
     
-# # if-else statement
+# if-else statement
 
-# n = int(input("enter a number: "))
-# if n%2 == 0:
-#     print("this nummber is even")
-# else:
-#     print("this number is odd")
+n = int(input("enter a number: "))
+if n%2 == 0:
+    print("this nummber is even")
+else:
+    print("this number is odd")
     
 
-#if-elif statement
+# if-elif statement
 
-# day = int(input("enter day: "))
+day = int(input("enter day: "))
 
-# if day == 1:
-#     print("monday")
-# elif day == 2:
-#     print("tuesday")
-# elif day == 3:
-#     print("wednesday")
-# elif day == 4:
-#     print("thursday")
-# elif day == 5:
-#     print("friday")
-# elif day == 6:
-#     print("saturday")
-# elif day == 7:
-#     print("sunday")
-# else:
-#     print("holiday!!!")
+if day == 1:
+    print("monday")
+elif day == 2:
+    print("tuesday")
+elif day == 3:
+    print("wednesday")
+elif day == 4:
+    print("thursday")
+elif day == 5:
+    print("friday")
+elif day == 6:
+    print("saturday")
+elif day == 7:
+    print("sunday")
+else:
+    print("holiday!!!")
     
     
     
     
 # if condition:
-    #code
-#else condition:
-    #code
+#     code
+# else condition:
+#     code
     
     
-# num = int(input("enter: "))
+num = int(input("enter: "))
     
-# if num > 0:
-#     print("positive number")
-# else:
-#     print("negative number")
+if num > 0:
+    print("positive number")
+else:
+    print("negative number")
 
 
 
-# num2 = int(input("enter: "))
-# if num2%2 == 0:
-#     print(f"{num2} is an even number")
-# else:
-#     print(f"{num2} is an odd number")
-#     # print(num2, "is an odd number")
+num2 = int(input("enter: "))
+if num2%2 == 0:
+    print(f"{num2} is an even number")
+else:
+    print(f"{num2} is an odd number")
+    # print(num2, "is an odd number")
     
     
     
 # if condition:
-        #code
+#         code
 # elif condition:
-        # code
+#         code
 # else:
-        #code
+#         code
         
-# marks = int(input("enter marks:"))
+marks = int(input("enter marks:"))
 
-# if marks >= 90:
-#     print("A grade")
-# elif 75 <= marks <= 89:
-#     print("B grade")
-# elif 60 <= marks <= 74:
-#     print("C grade")
-# elif 40 <= marks <= 59:
-#     print("D grade")
-# else:
-#     print("FAIL")
+if marks >= 90:
+    print("A grade")
+elif 75 <= marks <= 89:
+    print("B grade")
+elif 60 <= marks <= 74:
+    print("C grade")
+elif 40 <= marks <= 59:
+    print("D grade")
+else:
+    print("FAIL")
 
 
 DAY = int(input("enter a number: "))
--- 05_looping_statements.py
+++ 05_looping_statements.py
@@ -8,31 +8,31 @@
 #   - while loop
 
 
-# for i in range(5):
-#     print(i)   #counting starts from 0
+for i in range(5):
+    print(i)   #counting starts from 0
     
-# for i in range(101):
-#     print(i)   #0-100
+for i in range(101):
+    print(i)   #0-100
     
-# for i in range(1,6):
-#     print(i)   #inclusive so counts everything except 6
+for i in range(1,6):
+    print(i)   #inclusive so counts everything except 6
     
-# for i in range(1,10,2):  
-#     print(i)    #skips every second number + 10,  cuz inlusive
+for i in range(1,10,2):  
+    print(i)    #skips every second number + 10,  cuz inlusive
     
-# for i in range(1,7,4):
-#     print(i)
+for i in range(1,7,4):
+    print(i)
     
     
 # for i in range(start,stop,step):
 
-# n = int(input("give a number: "))
+n = int(input("give a number: "))
 
-# for i in range(2,n+1,2):  #starts from 2 for even
-#     print(i)
+for i in range(2,n+1,2):  #starts from 2 for even
+    print(i)
     
-# for i in range(1,n+1,2):  #starts from 1 for odd
-#     print(i)
+for i in range(1,n+1,2):  #starts from 1 for odd
+    print(i)
 
 
 n = int(input("enter a num: "))
@@ -42,3 +42,53 @@ for i in range(2,n+1, 2):
 print("odd numbers are: ")
 for i in range(1, n+1, 2):
     print(i)
+    
+    
+# 9/9/26
+    
+    
+
+n = int(input("enter any num:"))
+count = 0
+
+for i in range(1, n+1):
+    if i%2 == 0:
+        count = count + 1
+print(f" there are {count} even numbers")
+
+count2 = 0
+
+for i in range(1, n+1):
+    if i%2 != 0:
+        count2 = count2 + 1
+print(f"there are {count2} odd numbers")
+
+
+n = int(input("enter a numbers:"))
+sum = 0
+
+for i in range(1, n+1):
+     sum = sum + i
+print(sum)
+
+n = int(input("enter a num:"))
+sum = 0
+
+for i in range(2,n+1,2):   # start with 2 to consider all even numbers
+    sum = sum + i           # method 1
+print(sum)
+
+sum2 = 0
+
+for i in range(1,n+1): 
+    if   i%2 == 0:
+        sum2 = sum2 + i           # method 2
+print(sum2)
+
+
+n = int(input("enter a num:"))
+num = 1
+
+for i in range(1,n+1):
+    num = num*i
+print(num)
